9b605984f3fe8994a16a6e12653e0edc11a31ee5
xuexi007 <44258352+xuexi007@users.noreply.github.com>
2020-11-09 10:18:11 +0800

    Update demo.java

--- src/main/java/com/offcn/demo.java
+++ src/main/java/com/offcn/demo.java
@@ -5,4 +5,9 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("这事一个演示代码");
     }
+    
+    public void test(){
+         System.out.println("这事一个test代码");
+        
+    }
 }
