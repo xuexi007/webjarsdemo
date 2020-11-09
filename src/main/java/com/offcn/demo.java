6a97cb8ee90535acfd3e67f5d5aad0616d17af7d
mysunny <hk109@126.com>
2020-11-09 10:19:05 +0800

    修改demo.java

--- src/main/java/com/offcn/demo.java
+++ src/main/java/com/offcn/demo.java
@@ -5,7 +5,8 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("这事一个演示代码");
     }
-    
+
+    //增加注释
     public void test(){
          System.out.println("这事一个test代码");
         
